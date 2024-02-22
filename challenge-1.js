28f46a37def6c82b4221b2fcc00e71137b8253f6
bwardak <baheer.wardak06@gmail.com>
2024-02-22 02:46:14 +0000

    Completed first challenge, tested outputs

---
+++ challenge-1.js
@@ -0,0 +1,11 @@
+function isNumberPositive(number) {
+    return number >= 0
+}
+
+/* code I used to test the output 
+
+let positivetest = isNumberPositive(6)
+let negativetest = isNumberPositive(-3)
+
+console.log(positivetest)
+console.log(negativetest)*/
