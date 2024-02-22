b09495b0680765428a1a64d8d3ce05d3f7b7ecda
bwardak <baheer.wardak06@gmail.com>
2024-02-22 23:04:50 +0000

    Completed 2nd challenge

--- challenge-1.js
+++ challenge-1.js
@@ -2,7 +2,7 @@ function isNumberPositive(number) {
     return number >= 0
 }
 
-/* code I used to test the output 
+/* code I used to test the output incase i ever come to this doc again
 
 let positivetest = isNumberPositive(6)
 let negativetest = isNumberPositive(-3)
---
+++ challenge-2.js
@@ -0,0 +1,15 @@
+function convertDaysToAge(numberOfDays) {
+    let age, result;
+
+    age = numberOfDays/360;
+    return result = Math.trunc(age)
+    
+}
+
+/*  code to test output
+
+let test1 = convertDaysToAge(3650)
+let test2 = convertDaysToAge(6570)
+
+console.log(test1)
+console.log(test2) */
